752c99b5ea234b86b81f50ea63038b3b2a3d7204
Edsey-omenda <omendajames0317@gmail.com>
2023-02-13 11:12:49 +0300

    updated

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,5 +1,5 @@
 class CustomerSerializer < ActiveModel::Serializer
   attributes :id, :name, :account_type, :account_number, :email, :age, :contact
 
-  # has_many :accounts
+  has_many :accounts
 end
